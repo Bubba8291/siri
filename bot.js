defe5d7df4d90db9dfe80e7a5817921c662b04f8
Grant Gordinier <gameboygrant@gmail.com>
2020-07-10 12:37:21 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,22 +5,13 @@ const Constants = require('discord.js/src/util/Constants.js');
 const uuid = require('uuid');
 const ytdl = require('ytdl-core');
 const ytsr = require('ytsr');
+const https = require('https');
 
 Constants.DefaultOptions.ws.properties.$browser = `Discord iOS`;
 client.on("ready", () => {
         client.user.setActivity(".heysiri help");
 
-//	here2help();
-//	setInterval(here2help, 3600000);
 });
-
-function here2help() {
-	if(maintenance == false) {
-		client.channels.cache.get("297955830106554368").send("Don't forget, I'm always here to help! To get started, type `.heysiri help` in the bot commands channel.");
-	} else {
-		client.channels.cache.get("710402599509164113").send("Don't forget, I'm always here to help! To get started, type `.heysiri help` in the bot commands channel.");
-	}
-}
 
 var servers = [];
 
@@ -120,7 +111,8 @@ client.on('message', message => {
 					message.channel.send("You must have manage server permissions to perform this.");
 				}
 			} else if(parse.action == "music_player_control.skip_forward") {
-				if(message.member.permissions.has('ADMINISTRATOR') || message.member.permissions.has('MANAGE_GUILD')) {
+				if(message.member.voice.channel) {
+					var size = message.member.voice.channel.members.filter(x => !x.bot).size;
 					var server = servers[message.guild.id];
 					if(!server.peopleSkipped) {
 						server.peopleSkipped = [];
@@ -128,79 +120,41 @@ client.on('message', message => {
 					if(!server.skipCount) {
 						server.skipCount = 0;
 					}
-
-					if(message.member.voice.channel) {
-						if(!servers[message.guild.id]) {
-							message.channel.send("The server isn't currently playing any music.");
-							return;
-						} else if(!server.queue.playedBy[0]) {
-							message.channel.send("The server isn't currently playing any music.");
-							return;
-						}
-
-						server.queue.playedBy.shift();
+					if(!servers[message.guild.id]) {
+						message.channel.send("The server isn't currently playing any music.");
+						return;
+					} else if(!server.queue.playedBy[0]) {
+						message.channel.send("The server isn't currently playing any music.");
+						return;
+					}
+					if(message.member.id == server.queue.playedBy[0]) {
 						server.dispatcher.end();
-						server.peopleSkipped = [];
-						server.skipCount = 0;
 						message.channel.send("Music Skipped");
 					} else {
-						message.channel.send("You must be in a voice channel to skip music.");
-					}
-				} else {
-					if(message.member.voice.channel) {
-						var size = message.member.voice.channel.members.filter(x => !x.bot).size;
-						var server = servers[message.guild.id];
-						if(!server.peopleSkipped) {
-							server.peopleSkipped = [];
-						}
-						if(!server.skipCount) {
-							server.skipCount = 0;
-						}
-						if(!servers[message.guild.id]) {
-							message.channel.send("The server isn't currently playing any music.");
-							return;
-						} else if(!server.queue.playedBy[0]) {
-							message.channel.send("The server isn't currently playing any music.");
-							return;
-						}
-						if(message.member.id == server.queue.playedBy[0]) {
-							server.queue.playedBy.shift();
-							server.dispatcher.end();
-							server.peopleSkipped = [];
-							server.skipCount = 0;
-							message.channel.send("Music Skipped");
-						} else {
-							if(size >= 3) {
-								if(server.peopleSkipped.includes(message.member.id)) {
-									message.channel.send("You already voted to skip.");
-									return;
-								}
-								let skipsRequired = Math.round(size / 2);
-								server.skipCount++;
-								if(skipsRequired != server.skipCount) {
-									message.channel.send(server.skipCount + "/" + skipsRequired + " skips needed.");
-									peopleSkipped.push(message.member.id);
-								} else {
-									server.queue.playedBy.shift();
-									server.dispatcher.end();
-									server.skipCount = 0;
-									server.peopleSkipped = [];
-									message.channel.send("Music Skipped");
-								}
+						if(size >= 3) {
+							if(server.peopleSkipped.includes(message.member.id)) {
+								message.channel.send("You already voted to skip.");
+								return;
+							}
+							let skipsRequired = Math.round(size / 2);
+							server.skipCount++;
+							if(skipsRequired != server.skipCount) {
+								message.channel.send(server.skipCount + "/" + skipsRequired + " skips needed.");
+								server.peopleSkipped.push(message.member.id);
 							} else {
-								server.queue.playedBy.shift();
 								server.dispatcher.end();
-								server.skipCount = 0;
-								server.peopleSkipped = [];
 								message.channel.send("Music Skipped");
 							}
+						} else {
+							server.dispatcher.end();
+							message.channel.send("Music Skipped");
 						}
-					} else {
-						message.channel.send("You must be in a voice channel to skip music.");
 					}
+				} else {
+					message.channel.send("You must be in a voice channel to skip music.");
 				}
 			} else if(parse.action == "easteregg.uses") {
-				 message.channel.send("Current Features:\n```Get the Weather: .heysiri what's the weather in new york in C?\nTranslate Text: .heysiri translate Hello to Spanish?\nConvert Currencies: .heysiri convert 1 usd to rupees\nSearch the Web: .heysiri google discord.com\nTell a Joke: .heysiri tell me a joke\nPlay Music (experimental): .heysiri play Wonderwall by Oasis\nPause Music (experimental): .heysiri pause music\nSkip Music (experimental): .heysiri skip```");
+				 message.channel.send("Current Features:\n```Get the Weather: .heysiri what's the weather in new york in C?\nTranslate Text: .heysiri translate Hello to Spanish?\nConvert Currencies: .heysiri convert 1 usd to rupees\nSearch the Web: .heysiri google discord.com\nTell a Joke: .heysiri tell me a joke\nPlay Music: .play Wonderwall by Oasis\nPause Music: .heysiri pause music\nSkip Music: .skip\nForce Skip: .forceskip\nShow the music queue: .queue\nGet Coronavirus cases by country: .corona\nMake a suggestion: .suggest\nDad Joke: .dadjoke```");
 			} else if(parse.action == "easteregg.wwdccountdown") {
 				if(message.channel.id == 551487463625850882) {
 					message.channel.send("You're asking the wrong person!");
@@ -225,7 +179,7 @@ client.on('message', message => {
 			let targetChannel = client.channels.cache.get(id);
 			if(targetChannel) targetChannel.send(text);
 		}
-	} else if(command === "play") {
+	} else if(command === "play" || command === "p") {
 		let message2process = message.content.split(' ').splice(1).join(' ');
 		if(message.member.voice.channel) {
 			if(!servers[message.guild.id]) {
@@ -298,15 +252,237 @@ client.on('message', message => {
 						play(connection, message);
 					});
 
-										message.channel.send("Added " + jsonVideo.items[0].title + " to queue");
+					message.channel.send("Added " + jsonVideo.items[0].title + " to queue");
 				});
 			  });
 			});
 		} else {
 			message.channel.send("You must be in a voice channel to play music.");
 		}
-	}
+	} else if(command === "reboot") {
+		if(message.member.permissions.has('ADMINISTRATOR') || message.member.permissions.has('MANAGE_MESSAGES') || message.author.id == 706744803483975751) {
+			resetBot(message.channel);
+		} else {
+			message.channel.send("You must be a modmin to perform this.");
+		}
+	} else if(command === "queue" || command === "q") {
+		var server = servers[message.guild.id];
+		if(!servers[message.guild.id]) {
+			message.channel.send("The server isn't currently playing any music.");
+			return;
+                } else if(!server.queue.playedBy[0]) {
+			message.channel.send("The server isn't currently playing any music.");
+			return;
+                }
+
+		var message2send = "";
+		for(var i=0;i<server.queue.playedBy.length;i++) {
+			let guild = client.guilds.cache.get(message.guild.id);
+			let member = guild.member(server.queue.playedBy[i]);
+			let nickname = member ? member.displayName : null;
+			if(i == 0) {
+				message2send = "Current song played by " + nickname + "\n";
+			} else {
+				message2send = message2send + i + '. <' + server.queue.song[i - 1] + '> requested by ' + nickname + "\n";
+			}
+		}
+		message.channel.send(message2send);
+	} else if(command === "corona") {
+		let message2send = message.content.split(' ').splice(1).join(' ');
+
+		if(message2send == "") {
+			message.channel.send("Usage: .corona countries | .corona US");
+			return;
+		}
+		if(message2send == "countries") {
+	      https.get('https://covid2019-api.herokuapp.com/v2/current', (resp) => {
+        	let data = '';
+
+	        resp.on('data', (chunk) => {
+        	  data += chunk;
+	        });
+
+			resp.on('end', () => {
+			var parse = JSON.parse(data);
+			var msgg = "Available Countries\n";
+			for(location in parse.data) {
+				msgg += parse.data[location].location + "\n";
+			}
+			message.author.send(msgg);
+			message.channel.send("DMed you the list of valid countries.");
+			});
+
+			}).on("error", (err) => {
+				messagel.channel.send("Error getting country list for Coronavirus cases.");
+			});
+		} else {
+      		let encodedName = encodeURIComponent(message2send);
+			https.get('https://covid2019-api.herokuapp.com/v2/country/' + encodedName, (resp) => {
+			let data = '';
+
+				// A chunk of data has been recieved.
+			resp.on('data', (chunk) => {
+				data += chunk;
+			});
+
+				// The whole response has been received. Print out the result.
+			resp.on('end', () => {
+				var parse = JSON.parse(data);
+				var msgg = "";
+			if(!parse.data[0]) {
+				msgg = parse.data.location + "\nConfirmed Cases: " + numberWithCommas(parse.data.confirmed) + "\nDeath Count: " + numberWithCommas(parse.data.deaths) + "\nRecovered: " + numberWithCommas(parse.data.recovered) + "\nActive Cases: " + numberWithCommas(parse.data.active);
+			} else {
+				msgg = "Must supply a valid country. Use `.corona countries` to get a list of valid countries.";
+			}
+				message.channel.send(msgg);
+			});
+
+			}).on("error", (err) => {
+				messagel.channel.send("Error getting country for Coronavirus cases.");
+			});
+    	}
+	} else if(command === "suggest") {
+		let message2send = message.content.split(' ').splice(1).join(' ');
+		if(message2send == "") {
+			message.channel.send("Usage: `.suggest pizza` (replace pizza with a suggestion)");
+			return;
+		}
+
+		user = message.member.user.username;
+		colorrole = message.member.displayColor;
+
+		const embed = new Discord.MessageEmbed()
+			                        .setColor(colorrole)
+						.setTitle(user)
+						.setThumbnail(message.author.avatarURL())
+						.addField('Suggestion', message2send, true)
+						.setTimestamp()
+//		message.delete();
+		client.channels.cache.get("725407518054154261").send(embed);
+		message.channel.send("Thank you. Your suggestion has been submitted.");
+	} else if(command === "forceskip" || command === "fs") {
+		if(message.member.permissions.has('ADMINISTRATOR') || message.member.permissions.has('MANAGE_MESSAGES') || message.author.id == 706744803483975751) {
+			var server = servers[message.guild.id];
+
+			if(message.member.voice.channel) {
+				if(!server.peopleSkipped) {
+					server.peopleSkipped = [];
+				}
+				if(!server.skipCount) {
+					server.skipCount = 0;
+				}
+
+				if(!servers[message.guild.id]) {
+					message.channel.send("The server isn't currently playing any music.");
+					return;
+				} else if(!server.queue.playedBy[0]) {
+					message.channel.send("The server isn't currently playing any music.");
+					return;
+				} else {
+					server.dispatcher.end();
+					message.channel.send("Music Skipped");
+				}
+			} else {
+				message.channel.send("You must be in a voice channel to skip music.");
+			}
+		} else {
+			message.channel.send("You must be a modmin to perform this.");
+		}
+	} else if(command === "skip" || command === "s") {
+		if(message.member.voice.channel) {
+			var size = message.member.voice.channel.members.filter(x => !x.bot).size;
+			var server = servers[message.guild.id];
+			if(!server.peopleSkipped) {
+				server.peopleSkipped = [];
+			}
+			if(!server.skipCount) {
+				server.skipCount = 0;
+			}
+			if(!servers[message.guild.id]) {
+				message.channel.send("The server isn't currently playing any music.");
+				return;
+			} else if(!server.queue.playedBy[0]) {
+				message.channel.send("The server isn't currently playing any music.");
+				return;
+			}
+			if(message.member.id == server.queue.playedBy[0]) {
+				server.dispatcher.end();
+				message.channel.send("Music Skipped");
+			} else {
+				if(size >= 3) {
+					if(server.peopleSkipped.includes(message.member.id)) {
+						message.channel.send("You already voted to skip.");
+						return;
+					}
+					let skipsRequired = Math.round(size / 2);
+					server.skipCount++;
+					if(skipsRequired != server.skipCount) {
+						message.channel.send(server.skipCount + "/" + skipsRequired + " skips needed.");
+						server.peopleSkipped.push(message.member.id);
+					} else {
+						server.dispatcher.end();
+						message.channel.send("Music Skipped");
+					}
+				} else {
+					server.dispatcher.end();
+					message.channel.send("Music Skipped");
+				}
+			}
+		} else {
+			message.channel.send("You must be in a voice channel to skip music.");
+		}
+	} else if (command === 'dadjoke') {
+                var post_options = {
+                      host: 'icanhazdadjoke.com',
+                      port: '443',
+                      path: '/',
+                      method: 'GET',
+                      headers: {
+                          'Accept': 'application/json'
+                      }
+                  };
+                  var post_req = https.request(post_options, function(res) {
+                      res.on('data', function (chunk) {
+                          var parse = JSON.parse(chunk);
+                          if(parse.status == 200) {
+                                message.channel.send(parse.joke);
+                          } else {
+                                message.channel.send("Error getting dadjoke.");
+                          }
+                      });
+                  });
+
+                post_req.end();
+        }/* else if(command === 'pickupline') {
+                 var post_options = {
+                      host: 'pebble-pickup.herokuapp.com',
+                      port: '443',
+                      path: '/tweets/random',
+                      method: 'GET',
+                      headers: {
+                          'Accept': 'application/json'
+                      }
+                  };
+                  var post_req = https.request(post_options, function(res) {
+                      res.on('data', function (chunk) {
+                          var parse = JSON.parse(chunk);
+			  message.channel.send(parse.tweet);
+                      });
+                  });
+
+                post_req.end();
+        }*/
 });
+
+function numberWithCommas(x) {
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function resetBot(channel) {
+    channel.send('Restarting bot...')
+    .then(msg => client.destroy())
+    .then(() => client.login(''));
+}
 
 function play(connection, message) {
 	var server = servers[message.guild.id];
@@ -315,6 +491,9 @@ function play(connection, message) {
 	server.queue.song.shift();
 	server.dispatcher.on("finish", function() {
 		server.queue.playedBy.shift();
+		server.skipCount = 0;
+		server.peopleSkipped = [];
+
 		if(server.queue.song[0]) {
 			console.log("Next song");
 			play(connection, message);
